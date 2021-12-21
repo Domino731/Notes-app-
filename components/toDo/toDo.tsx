21e6b5d49bc6aba212f2384847a06792811c058a
Domino731 <d.orzechowski18@gmail.com>
2021-12-21 15:26:15 +0100

    toDo - changing text state & listening for task lenght

--- components/toDo/toDo.tsx
+++ components/toDo/toDo.tsx
@@ -1,45 +1,68 @@
 import React, { FunctionComponent, useState } from "react";
-import { View ,  StyleSheet, TextInput, Button} from "react-native";
+import {
+  View,
+  StyleSheet,
+  TextInput,
+  Button,
+  TouchableOpacity,
+  Text
+} from "react-native";
 
-export const ToDo : FunctionComponent = () => {
-
+export const ToDo: FunctionComponent = () => {
   const [data, setData] = useState<string[]>([]);
 
-  const [taskText, setTaskText] = useState<string>('');
+  const [taskText, setTaskText] = useState<string>("");
 
+  const [flag, setFlag] = useState<boolean>(false);
 
-  
   const handleAddTask = () => {
-    setData( prev => [...prev, taskText]);
-    return setTaskText('');
+    setData((prev) => [...prev, taskText]);
+    return setTaskText("");
+  };
+
+  const handleSetTaskText = (data: string) => {
+    data.length > 0 ? setFlag(true) : setFlag(false) ;
+    return setTaskText(data);
   }
-  
- return <View style={styles.container}> 
-    <TextInput
+  return (
+    <View style={styles.container}>
+      <TextInput
         style={styles.input}
         defaultValue="You can type in me"
-        onChangeText={setTaskText}
+        onChangeText={handleSetTaskText}
         value={taskText}
       />
-      <Button
-  onPress={handleAddTask}
-  title="Learn More"
-  color="#841584"
-  accessibilityLabel="Learn more about this purple button"
-/>
- </View>
-}
+      {flag && <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>Add new task</Text>
+      </TouchableOpacity>}
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-     paddingTop: 20
-    },
-      input: {
-          backgroundColor: '#fff',
-          fontSize: 20,
-          height: 40,
-          paddingLeft: 10,
-          paddingRight: 10,
-          borderRadius: 8
-      }
+  container: {
+    paddingTop: 20,
+  },
+  input: {
+    backgroundColor: "#fff",
+    fontSize: 20,
+    height: 40,
+    paddingLeft: 10,
+    paddingRight: 10,
+    borderRadius: 8,
+  },
+  button: {
+    marginTop: 12,
+    width: '100%',
+    paddingTop: 6,
+    paddingBottom: 6,
+    backgroundColor: '#f72585',
+    borderRadius: 8,
+  },
+  buttonText: {
+    fontSize: 20,
+    textAlign: 'center',
+    color: '#fff',
+    fontWeight: 'bold',
+  }
 });
